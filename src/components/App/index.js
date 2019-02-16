7bbe4003b10de2da2c49bb5c1ccfabcacef36733
uyouthe <melvoloskov@ya.ru>
2019-02-16 18:48:39 +0300

    cat moved to header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import styles from './styles.module.css'
 import Chart from '../Chart'
+import Header from '../Header'
 import Tags from '../Tags'
 import Footer from '../Footer'
-import { ReactComponent as Cat } from './cat.svg'
+import data from '../../popularity.json'
+import labelDay from './days.js'
 
 export default props => (
   <main className={ styles.root }>
-    <Cat className={ styles.cat } />
-    <Chart />
+    <Header day={ labelDay(props.date.getDay()) }/>
+    <Chart data={ data } date={ props.date } />
     <Tags />
     <Footer />
   </main>
