247b7dca53dd8fc3a7e34ba115a0a6d278b87652
uyouthe <melvoloskov@ya.ru>
2019-02-16 23:11:08 +0300

    service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import 'modern-normalize'
 import './index.css'
 import App from './App'
-// import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-// serviceWorker.unregister()
+serviceWorker.register()
