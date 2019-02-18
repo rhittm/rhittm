3e79acbc9e6f3d17f4ce17d7b67c5a80d18c19c3
uyouthe <melvoloskov@ya.ru>
2019-02-18 19:59:58 +0300

    basic presets editor

--- src/Presets/index.js
+++ src/Presets/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Preset from '../Preset'
+import { Picker } from 'emoji-mart'
+import { Tooltip } from 'react-tippy'
 import styles from './styles.module.css'
 
 class Presets extends Component {
@@ -23,6 +25,45 @@ class Presets extends Component {
               { preset }
             </Preset>
         )) }
+        <h2 className={ styles.heading }>New Preset</h2>
+        <div className={ styles.container }>
+          <div className={ styles.sidebar }>
+            <Picker
+              title="Pick an emoji..."
+              showPreview={ false }
+              showSkinTones={ false }
+              native
+              style={{
+                width: '100%',
+                borderColor: '#ccc'
+              }}
+            />
+          </div>
+          <div className={ styles.main }>
+            <Preset
+              title="🤷"
+              color="#feb692"
+              inline
+            >
+              #hello
+            </Preset>
+            <textarea className={ styles.content }>
+              #hello
+            </textarea>
+            <Tooltip
+              title="Saved!"
+              trigger="click"
+              disabled={ false }
+            >
+              <button
+                type="button"
+                className={ styles.button }
+                >
+                  Save
+                </button>
+            </Tooltip>
+          </div>
+        </div>
       </div>
     )
   }
